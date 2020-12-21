bb5fb5fa3868c82d4e9558d9a354335e73023cda
jdkwak1994 <jdkwak1994@gmail.com>
2020-12-20 21:28:29 -0500

    map data completed

--- templates/static/js/logic.js
+++ templates/static/js/logic.js
@@ -1,4 +1,13 @@
-var myMap = L.map('mapid').setView([30.0444, 31.2357], 2);
+var myMap = L.map('mapid', {minZoom: 3}).setView([30.0444, 31.2357], 3);
+
+var southWest = L.latLng(-89.98155760646617, -180),
+northEast = L.latLng(89.99346179538875, 180);
+var bounds = L.latLngBounds(southWest, northEast);
+
+myMap.setMaxBounds(bounds);
+myMap.on("drag", function() {
+  myMap.panInsideBounds(bounds, {animate: false});
+});
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,6 +23,7 @@ var long = [];
 var womanname = [];
 var womanrole = [];
 var womanpic = [];
+var desc = [];
 
 mapdata.forEach((mapdata) => {
   lat.push(parseFloat(mapdata.Latitude));
@@ -21,11 +31,12 @@ mapdata.forEach((mapdata) => {
   womanname.push(mapdata.name);
   womanrole.push(mapdata.role);
   womanpic.push(mapdata.img);
+  desc.push(mapdata.description);
 });
 
 var i;
 for (i = 1; i < mapdata.length; i++) {
   L.marker([lat[i], long[i]]).bindPopup(
-    '<img src="' + womanpic[i] + '" width="100" height="100"/><br><strong>' + womanname[i] + '</strong><br>' + womanrole[i]
+    '<img src="' + womanpic[i] + '" width="150" height="150"/><br><strong>' + womanname[i] + '</strong><br><i>' + womanrole[i] + '</i><br><br>' + desc[i]
     ).addTo(myMap);
 };
